b856619601582b3e3c1cf7ea38b715b559b89ca4
Brian <musyimib449@gmail.com>
2025-05-05 21:54:02 +0300

    python function lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print(f"Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return(num1 + num2)
+
+
 
 def halve(number):
-    pass
+    if type(number) == int or type(number) == float:
+        return number / 2
+    return None
+
